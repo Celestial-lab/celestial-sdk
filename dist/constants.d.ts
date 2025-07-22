8312cb8c1040a5e19e1b5d7da83e0f9df5aae6b2
xuantan123 <phanxuantan123@gmail.com>
2025-07-22 14:24:35 +0700

    updatecode

--- dist/constants.d.ts
+++ dist/constants.d.ts
@@ -2,7 +2,7 @@ import JSBI from 'jsbi';
 export declare type BigintIsh = JSBI | bigint | string;
 export declare enum ChainId {
     MAINNET = 7000,
-    TESTNET = 10143
+    TESTNET = 6342
 }
 export declare enum TradeType {
     EXACT_INPUT = 0,
--- dist/entities/token.d.ts
+++ dist/entities/token.d.ts
@@ -27,5 +27,5 @@ export declare class Token extends Currency {
 export declare function currencyEquals(currencyA: Currency, currencyB: Currency): boolean;
 export declare const WETH: {
     7000: Token;
-    10143: Token;
+    6342: Token;
 };
